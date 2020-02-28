97c73f1ae594ffb2cb6743f1313a696b9c77451d
brinxmat <rurik.greenall@gmail.com>
2020-02-28 12:06:12 +0100

    Add message to error

--- src/main/java/no/unit/nva/model/Role.java
+++ src/main/java/no/unit/nva/model/Role.java
@@ -4,9 +4,15 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.util.Arrays;
 
+import static java.lang.String.format;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+
 public enum Role {
     CREATOR("Creator");
 
+    public static final String ERROR_MESSAGE_TEMPLATE = "%s not a valid Role, expected one of: %s";
+    public static final String DELIMITER = ", ";
     private String value;
 
     Role(String value) {
@@ -25,10 +31,11 @@ public enum Role {
      * @return  enum
      */
     public static Role lookup(String value) {
-        return Arrays
-                .stream(values())
+        return stream(values())
                 .filter(nameType -> nameType.getValue().equalsIgnoreCase(value))
                 .findAny()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException(
+                        format(ERROR_MESSAGE_TEMPLATE, value, stream(Role.values())
+                                .map(Role::toString).collect(joining(DELIMITER)))));
     }
 }
