fcde45c3d0df6638d87ba4e29df1b1676e433917
brinxmat <rurik.greenall@gmail.com>
2020-03-18 11:38:13 +0100

    Fix field access, update equals

--- src/main/java/no/unit/nva/model/EntityDescription.java
+++ src/main/java/no/unit/nva/model/EntityDescription.java
@@ -99,7 +99,8 @@ public class EntityDescription {
                 && Objects.equals(getLanguage(), that.getLanguage())
                 && Objects.equals(getDate(), that.getDate())
                 && Objects.equals(getContributors(), that.getContributors())
-                && Objects.equals(getAbstract(), that.getAbstract());
+                && Objects.equals(getAbstract(), that.getAbstract())
+                && Objects.equals(getNpiSubjectHeading(), that.getNpiSubjectHeading());
     }
 
     @Override
@@ -125,8 +126,8 @@ public class EntityDescription {
     }
 
     public static final class Builder {
-        public String mainLanguageAbstract;
-        public String npiSubjectHeading;
+        private String mainLanguageAbstract;
+        private String npiSubjectHeading;
         private PublicationType publicationType;
         private String mainTitle;
         private Map<String, String> alternativeTitles;
