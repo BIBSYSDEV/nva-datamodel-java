e5b544e6c3e6d1db26b9f975403884429c79256e
brinxmat <rurik.greenall@gmail.com>
2020-05-11 07:59:50 +0200

    Linting and add javadoc

--- src/main/java/no/unit/nva/model/exceptions/InvalidPageTypeException.java
+++ src/main/java/no/unit/nva/model/exceptions/InvalidPageTypeException.java
@@ -6,6 +6,15 @@ import no.unit.nva.model.pages.Pages;
 public class InvalidPageTypeException extends Exception {
     public static final String INVALID_CLASS_MESSAGE =
             "Class %s expects a Pages object of type %s, while an object of type %s was provided";
+
+    /**
+     * This exception is thrown when a Pages object is used with a class that expects a specific Pages type. For example:
+     * JournalArticle is compatible with a Pages object of type Range.
+     *
+     * @param publicationInstance The publication instance.
+     * @param expectedPages The expected Pages type.
+     * @param actualPages The actual Pages type.
+     */
     public InvalidPageTypeException(Class<? extends PublicationInstance> publicationInstance,
                                     Class<? extends Pages> expectedPages,
                                     Class<? extends Pages> actualPages) {
