7258c8483e59cb085e91ab79d17bc847759c1f5b
brinxmat <rurik.greenall@gmail.com>
2020-05-21 17:25:00 +0200

    Add javadoc

--- src/test/java/no/unit/nva/model/util/JournalNonPeerReviewedContentUtil.java
+++ src/test/java/no/unit/nva/model/util/JournalNonPeerReviewedContentUtil.java
@@ -2,6 +2,18 @@ package no.unit.nva.model.util;
 
 public class JournalNonPeerReviewedContentUtil {
 
+    /**
+     * Creates a JSON string that matches subclasses of JournalNonPeerReviewedContent.
+     *
+     * @param type          The type of subclass of JournalNonPeerReviewedContent.
+     * @param volume        Volume of the content.
+     * @param issue         Issue of the content.
+     * @param articleNumber Article number of the content.
+     * @param begin         The begin page number.
+     * @param end           The end page number.
+     * @param peerReviewed  The peer-review boolean. Can be set to true for testing purposes.
+     * @return A formated JSON string.
+     */
     public static String generateJsonString(String type,
                                             String volume,
                                             String issue,
