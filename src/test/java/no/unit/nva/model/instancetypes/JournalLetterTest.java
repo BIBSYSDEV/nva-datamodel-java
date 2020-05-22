c4d76c6db96291cc8a575435ae00f4a16470a70e
brinxmat <rurik.greenall@gmail.com>
2020-05-22 12:33:21 +0200

    Move object mapper to static variable

--- src/test/java/no/unit/nva/model/instancetypes/JournalLetterTest.java
+++ src/test/java/no/unit/nva/model/instancetypes/JournalLetterTest.java
@@ -1,6 +1,7 @@
 package no.unit.nva.model.instancetypes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import no.unit.nva.model.exceptions.InvalidPageTypeException;
 import no.unit.nva.model.pages.Range;
 import nva.commons.utils.IoUtils;
@@ -16,14 +17,15 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class JournalLetterTest {
 
     public static final String JOURNAL_LETTER_WITH_PEER_REVIEW_TRUE_JSON = "journal_letter.json";
+    public static final ObjectMapper objectMapper = JsonUtils.objectMapper;
 
     @DisplayName("Journal letters to editor can be created from JSON")
     @Test
     void journalLetterReturnsObjectWhenJsonInputIsCorrectlySerialized() throws JsonProcessingException,
             InvalidPageTypeException {
         JournalLetter expected = generateJournalLetter("1", "3", "123", "2", "3");
-        String json = JsonUtils.objectMapper.writeValueAsString(expected);
-        JournalLetter journalLetter = JsonUtils.objectMapper.readValue(json, JournalLetter.class);
+        String json = objectMapper.writeValueAsString(expected);
+        JournalLetter journalLetter = objectMapper.readValue(json, JournalLetter.class);
         assertEquals(expected, journalLetter);
     }
 
@@ -32,7 +34,7 @@ class JournalLetterTest {
     void journalLetterSetsPeerReviewedToFalseWhenPeerReviewIsTrue() throws JsonProcessingException {
         String json = IoUtils.stringFromResources(Path.of(
                 JOURNAL_LETTER_WITH_PEER_REVIEW_TRUE_JSON));
-        assertFalse(JsonUtils.objectMapper.readValue(json, JournalLetter.class).isPeerReviewed());
+        assertFalse(objectMapper.readValue(json, JournalLetter.class).isPeerReviewed());
     }
 
     private JournalLetter generateJournalLetter(String volume,
