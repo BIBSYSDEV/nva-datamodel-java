61d732e3da621a6434ee9ca95a4a573961c54e52
Jan bjørndalen <jan.bjorndalen@gmail.com>
2020-05-25 09:11:18 +0200

    Code review readability fix

--- src/main/java/no/unit/nva/WithIndex.java
+++ src/main/java/no/unit/nva/WithIndex.java
@@ -2,6 +2,8 @@ package no.unit.nva;
 
 import static java.util.Objects.isNull;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.Optional;
 import no.unit.nva.model.Organization;
 
@@ -15,10 +17,11 @@ public interface WithIndex extends WithInternal {
      * @return publisherId
      */
     default String getPublisherId() {
-        Organization publisher = Optional.ofNullable(getPublisher()).orElseThrow(
-            () -> new IllegalStateException("Object publisher can not be null"));
-        return Optional.ofNullable(publisher.getId()).orElseThrow(
-            () -> new IllegalStateException("Property publisher.id can not be null")).toString();
+        Organization publisher = Optional.ofNullable(getPublisher())
+            .orElseThrow(() -> new IllegalStateException("Object publisher can not be null"));
+        return Optional.ofNullable(publisher.getId())
+            .map(URI::toString)
+            .orElseThrow(() -> new IllegalStateException("Property publisher.id can not be null"));
     }
 
     /**
@@ -27,13 +30,12 @@ public interface WithIndex extends WithInternal {
      * @return publisherOwnerDate
      */
     default String getPublisherOwnerDate() {
-        return String.join(DYNAMODB_KEY_DELIMITER,
-            getPublisherId(),
-            Optional.ofNullable(getOwner()).orElseThrow(
-                () -> new IllegalStateException("Property owner can not be null")),
-            Optional.ofNullable(getModifiedDate()).orElseThrow(
-                () -> new IllegalStateException("Property modifiedDate can not be null")).toString()
-        );
+        String owner = Optional.ofNullable(getOwner())
+            .orElseThrow(() -> new IllegalStateException("Property owner can not be null"));
+        String date = Optional.ofNullable(getModifiedDate())
+            .map(Instant::toString)
+            .orElseThrow(() -> new IllegalStateException("Property modifiedDate can not be null"));
+        return String.join(DYNAMODB_KEY_DELIMITER, getPublisherId(), owner, date);
     }
 
     /**
@@ -45,12 +47,11 @@ public interface WithIndex extends WithInternal {
         if (isNull(getDoiRequest())) {
             return null; // not indexed
         }
-        return String.join(DYNAMODB_KEY_DELIMITER,
-            Optional.ofNullable(getDoiRequest().getStatus()).orElseThrow(
-                () -> new IllegalStateException("Property doiRequest.status can not be null")).toString(),
-            Optional.ofNullable(getDoiRequest().getDate()).orElseThrow(
-                () -> new IllegalStateException("Property doiRequest.date can not be null")).toString()
-        );
-    }
+        String statusString = Optional.ofNullable(getDoiRequest().getStatus()).map(Enum::toString)
+            .orElseThrow(() -> new IllegalStateException("Property doiRequest.status can not be null"));
+        String dateString = Optional.ofNullable(getDoiRequest().getDate()).map(Instant::toString)
+            .orElseThrow(() -> new IllegalStateException("Property doiRequest.date can not be null"));
 
+        return String.join(DYNAMODB_KEY_DELIMITER, statusString, dateString);
+    }
 }
