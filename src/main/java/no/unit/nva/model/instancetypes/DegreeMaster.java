592360985eb9acc24a1c20454a885ae0ea0430db
brinxmat <rurik.greenall@gmail.com>
2020-06-01 11:04:15 +0200

    Add degree master

---
+++ src/main/java/no/unit/nva/model/instancetypes/DegreeMaster.java
@@ -0,0 +1,47 @@
+package no.unit.nva.model.instancetypes;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import no.unit.nva.model.exceptions.InvalidPageTypeException;
+import no.unit.nva.model.pages.Pages;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+public class DegreeMaster extends BookMonograph {
+    public DegreeMaster() {
+        super();
+    }
+
+    private DegreeMaster(Builder builder) throws InvalidPageTypeException {
+        super();
+        setPages(builder.pages);
+        setPeerReviewed(builder.peerReviewed);
+        setOpenAccess(builder.openAccess);
+    }
+
+    public static final class Builder {
+        private Pages pages;
+        private boolean peerReviewed;
+        private boolean openAccess;
+
+        public Builder() {
+        }
+
+        public Builder withPages(Pages pages) {
+            this.pages = pages;
+            return this;
+        }
+
+        public Builder withPeerReviewed(boolean peerReviewed) {
+            this.peerReviewed = peerReviewed;
+            return this;
+        }
+
+        public Builder withOpenAccess(boolean openAccess) {
+            this.openAccess = openAccess;
+            return this;
+        }
+
+        public DegreeMaster build() throws InvalidPageTypeException {
+            return new DegreeMaster(this);
+        }
+    }
+}
