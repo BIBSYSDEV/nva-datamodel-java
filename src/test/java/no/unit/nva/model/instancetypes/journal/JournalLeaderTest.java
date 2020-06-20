c571222da78bbf500abe32875da8b4e6bd16ea5e
brinxmat <rurik.greenall@gmail.com>
2020-06-20 15:13:50 +0200

    Remove more stray imports

--- src/test/java/no/unit/nva/model/instancetypes/journal/JournalLeaderTest.java
+++ src/test/java/no/unit/nva/model/instancetypes/journal/JournalLeaderTest.java
@@ -7,7 +7,6 @@ import no.unit.nva.model.exceptions.InvalidPageRangeException;
 import no.unit.nva.model.instancetypes.InstanceTest;
 import no.unit.nva.model.instancetypes.JournalTestData;
 import no.unit.nva.model.instancetypes.NonPeerReviewed;
-import no.unit.nva.model.instancetypes.journal.JournalLeader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
