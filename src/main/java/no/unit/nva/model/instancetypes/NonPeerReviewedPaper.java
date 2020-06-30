2929226233bbafe8c31790e1c6cd602049a92d79
brinxmat <rurik.greenall@gmail.com>
2020-06-30 16:40:35 +0200

    Add equals and hashcode

--- src/main/java/no/unit/nva/model/instancetypes/NonPeerReviewedPaper.java
+++ src/main/java/no/unit/nva/model/instancetypes/NonPeerReviewedPaper.java
@@ -1,6 +1,9 @@
 package no.unit.nva.model.instancetypes;
 
 import no.unit.nva.model.pages.Range;
+import nva.commons.utils.JacocoGenerated;
+
+import java.util.Objects;
 
 public class NonPeerReviewedPaper extends NonPeerReviewed<Range> {
     private Range pages;
@@ -22,5 +25,24 @@ public class NonPeerReviewedPaper extends NonPeerReviewed<Range> {
     @Override
     public void setPages(Range pages) {
         this.pages = pages;
+    }
+
+    @JacocoGenerated
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof NonPeerReviewedPaper)) {
+            return false;
+        }
+        NonPeerReviewedPaper that = (NonPeerReviewedPaper) o;
+        return Objects.equals(getPages(), that.getPages());
+    }
+
+    @JacocoGenerated
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPages());
     }
 }
