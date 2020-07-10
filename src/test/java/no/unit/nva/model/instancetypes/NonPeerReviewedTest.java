f487ee6ed5dafbb0bdb5c2aa685e43a7f0658c27
brinxmat <rurik.greenall@gmail.com>
2020-07-10 14:07:27 +0200

    Migrate UnexpectedException -> IllegalArgumentException

--- src/test/java/no/unit/nva/model/instancetypes/NonPeerReviewedTest.java
+++ src/test/java/no/unit/nva/model/instancetypes/NonPeerReviewedTest.java
@@ -5,8 +5,10 @@ import no.unit.nva.model.pages.Range;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import java.rmi.UnexpectedException;
-
+import static no.unit.nva.model.instancetypes.NonPeerReviewed.PEER_REVIEWED_ERROR_TEMPLATE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NonPeerReviewedTest {
@@ -16,7 +18,10 @@ class NonPeerReviewedTest {
     public void nonPeerReviewedMonographPagesSettingPeerReviewedTrueThrowsUnexpectedException() {
         NonPeerReviewed<MonographPages> nonPeerReviewed = new NonPeerReviewedMonograph();
         Executable executable = () -> nonPeerReviewed.setPeerReviewed(true);
-        assertThrows(UnexpectedException.class, executable);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, executable);
+        String expectedMessage = String.format(PEER_REVIEWED_ERROR_TEMPLATE,
+                NonPeerReviewedMonograph.class.getSimpleName());
+        assertThat(exception.getMessage(), is(equalTo(expectedMessage)));
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -24,6 +29,9 @@ class NonPeerReviewedTest {
     public void nonPeerReviewedRangeSettingPeerReviewedTrueThrowsUnexpectedException() {
         NonPeerReviewed<Range> nonPeerReviewed = new NonPeerReviewedPaper();
         Executable executable = () -> nonPeerReviewed.setPeerReviewed(true);
-        assertThrows(UnexpectedException.class, executable);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, executable);
+        String expectedMessage = String.format(PEER_REVIEWED_ERROR_TEMPLATE,
+                NonPeerReviewedPaper.class.getSimpleName());
+        assertThat(exception.getMessage(), is(equalTo(expectedMessage)));
     }
 }
