ea57bdf9aea07d3226c35b2224e88539bf4d2240
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-09-16 16:10:53 +0200

    NP-1046: assert exception message

--- src/test/java/no/unit/nva/model/PublicationTest.java
+++ src/test/java/no/unit/nva/model/PublicationTest.java
@@ -1,7 +1,6 @@
 package no.unit.nva.model;
 
 import static no.unit.nva.model.DoiRequestStatus.APPROVED;
-import static no.unit.nva.model.DoiRequestStatus.ERROR_MESSAGE_NOT_ALLOWED_TO_CHANGE_STATUS_FROM_S_TO_S;
 import static no.unit.nva.model.DoiRequestStatus.REJECTED;
 import static no.unit.nva.model.DoiRequestStatus.REQUESTED;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -25,26 +24,11 @@ import no.unit.nva.model.util.PublicationGenerator;
 import org.junit.jupiter.api.Test;
 
 public class PublicationTest {
+
     public static final String PUBLICATION_CONTEXT_JSON = "src/main/resources/publicationContext.json";
     public static final String PUBLICATION_FRAME_JSON = "src/main/resources/publicationFrame.json";
 
     protected static final ObjectMapper objectMapper = nva.commons.utils.JsonUtils.objectMapper;
-
-    protected JsonNode toPublicationWithContext(Publication publication) throws IOException {
-        JsonNode document = objectMapper.readTree(objectMapper.writeValueAsString(publication));
-        JsonNode context = objectMapper.readTree(new FileInputStream(PUBLICATION_CONTEXT_JSON));
-        ContextUtil.injectContext(document, context);
-        return document;
-    }
-
-    protected Object produceFramedPublication(JsonNode publicationWithContext) throws IOException {
-        Object input = JsonUtils.fromString(objectMapper.writeValueAsString(publicationWithContext));
-        Object frame = JsonUtils.fromInputStream(new FileInputStream(PUBLICATION_FRAME_JSON));
-        JsonLdOptions options = new JsonLdOptions();
-        options.setOmitGraph(true);
-        options.setPruneBlankNodeIdentifiers(true);
-        return JsonLdProcessor.frame(input, frame, options);
-    }
 
     @Test
     public void updatingDoiStatusSuccessfullyChangesToValidNewDoiStatus()
@@ -64,7 +48,17 @@ public class PublicationTest {
         throws InvalidIssnException, MalformedContributorException {
         var publication = generatePublicationWithRejectedDoiRequestStatus();
 
-        assertThrows(IllegalArgumentException.class, () -> publication.updateDoiRequestStatus(REQUESTED));
+        var actualMessage = assertThrows(IllegalArgumentException.class,
+            () -> publication.updateDoiRequestStatus(REQUESTED))
+            .getMessage();
+
+        assertThat(actualMessage, containsString("not allowed"));
+
+        String statusInRequest = REQUESTED.toString();
+        assertThat(actualMessage, containsString(statusInRequest));
+
+        String initialStatus = REJECTED.toString();
+        assertThat(actualMessage, containsString(initialStatus));
     }
 
     @Test
@@ -76,6 +70,22 @@ public class PublicationTest {
             () -> publication.updateDoiRequestStatus(REQUESTED));
         assertThat(actualException.getMessage(),
             is(equalTo(Publication.ERROR_MESSAGE_UPDATEDOIREQUEST_MISSING_DOIREQUEST)));
+    }
+
+    protected JsonNode toPublicationWithContext(Publication publication) throws IOException {
+        JsonNode document = objectMapper.readTree(objectMapper.writeValueAsString(publication));
+        JsonNode context = objectMapper.readTree(new FileInputStream(PUBLICATION_CONTEXT_JSON));
+        ContextUtil.injectContext(document, context);
+        return document;
+    }
+
+    protected Object produceFramedPublication(JsonNode publicationWithContext) throws IOException {
+        Object input = JsonUtils.fromString(objectMapper.writeValueAsString(publicationWithContext));
+        Object frame = JsonUtils.fromInputStream(new FileInputStream(PUBLICATION_FRAME_JSON));
+        JsonLdOptions options = new JsonLdOptions();
+        options.setOmitGraph(true);
+        options.setPruneBlankNodeIdentifiers(true);
+        return JsonLdProcessor.frame(input, frame, options);
     }
 
     private Publication getPublicationWithoutDoiRequest() throws InvalidIssnException, MalformedContributorException {
