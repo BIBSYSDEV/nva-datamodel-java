7e0a605e5f954cc9ee6f80a95b0c60d3654641f8
svenngjeto <svenn.gjeto@unit.no>
2021-07-08 12:48:25 +0200

    Updated javadoc for linkedContext from comments and checkstyle

--- src/main/java/no/unit/nva/model/contexttypes/LinkedContext.java
+++ src/main/java/no/unit/nva/model/contexttypes/LinkedContext.java
@@ -6,7 +6,8 @@ import java.net.URI;
  * LinkedContexts are contexts that are expressed as a URI, allowing an existing (external) Publication
  * to be used as a PublicationContext. For example, a BookAnthology containing a Chapter, then the
  * Chapter will contain a linkedContext as a reference to the Book. A publication part of or included in another
- * publication will have an linkedContext referencing the containing publication - The linkedContext is always X isPartOf Y.
+ * publication will have an linkedContext referencing the containing publication
+ * The linkedContext is always X isPartOf Y.
  */
 public interface LinkedContext extends PublicationContext {
 
