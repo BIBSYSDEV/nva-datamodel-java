955e9ee5154b435d206ef59ad41c2ef11644d53c
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-08-11 14:38:13 +0200

    NP-2929: add series uri to book

--- src/test/java/no/unit/nva/model/contexttypes/BookTest.java
+++ src/test/java/no/unit/nva/model/contexttypes/BookTest.java
@@ -1,45 +1,91 @@
 package no.unit.nva.model.contexttypes;
 
-
+import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValues;
+import static no.unit.nva.model.util.PublicationGenerator.convertIsbnStringToList;
+import static no.unit.nva.utils.RandomPublicationContexts.randomBook;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import no.unit.nva.model.Level;
 import no.unit.nva.model.ModelTest;
 import no.unit.nva.model.exceptions.InvalidIsbnException;
+import no.unit.nva.model.exceptions.InvalidSeriesException;
 import nva.commons.core.JsonUtils;
+import org.apache.commons.validator.routines.ISBNValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static no.unit.nva.model.util.PublicationGenerator.convertIsbnStringToList;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BookTest extends ModelTest {
 
     public static final ObjectMapper objectMapper = JsonUtils.objectMapper;
     public static final String BOOK = "Book";
     public static final URI SAMPLE_LINKED_CONTEXT = URI.create("http://example.com/context");
+    public static final ISBNValidator ISBN_VALIDATOR = ISBNValidator.getInstance();
+
+    @Test
+    public void bookHasNonEmptySeriesUriWhenBookIsPartOfSeries() throws MalformedURLException, InvalidIsbnException {
+        Book book = randomBook();
+        assertThat(book.getSeriesUri(), is(not(nullValue())));
+        assertThat(book, doesNotHaveEmptyValues());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "https://api.dev.nva.aws.unit.no/publication-channels/journal/449575/2020",
+        "https://api.test.nva.aws.unit.no/publication-channels/journal/449575/2020",
+        "https://api.nva.aws.unit.no/publication-channels/journal/449575/2020",
+    })
+    public void bookAcceptsValidSeriesUri(String validSeriesUri) {
+        Book book =
+            assertDoesNotThrow((() -> randomBook().copy().withSeriesUri(URI.create(validSeriesUri)).build()));
+        assertThat(book.getSeriesUri().toString(), is(equalTo(validSeriesUri)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "https://api.dev.nva.unit.no/publication-channels/journal/449575/2020",
+        "http://www.example.com/",
+        "uri:not:http:uri"
+    })
+    public void bookThrowsExceptionWhenSeriesUriIsInvalid(String invalidSeriesUri) {
+        Executable action = ()-> randomBook().copy().withSeriesUri(URI.create(invalidSeriesUri)).build();
+        InvalidSeriesException exception= assertThrows(InvalidSeriesException.class,action);
+        assertThat(exception.getMessage(), containsString(invalidSeriesUri));
+    }
+
+    @Test
+    public void copyReturnsBookEqualToOriginalButDifferentObject() throws MalformedURLException, InvalidIsbnException {
+        Book original = randomBook();
+        assertThat(original, doesNotHaveEmptyValues());
+        Book copy = original.copy().build();
+        assertThat(copy, is(equalTo(original)));
+    }
 
     @DisplayName("Book can deserialize a book")
     @ParameterizedTest
     @CsvSource({
-            "A series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
-            ",, Full publisher details, LEVEL_2, true, true, \"9780201309515|9788131700075\"",
-            "A series title,,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
-            "Fulong of Oolong,12,T Publishing,LEVEL_0,false,false,\"9780201309515|9788131700075\""
+        "A series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
+        ",, Full publisher details, LEVEL_2, true, true, \"9780201309515|9788131700075\"",
+        "A series title,,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
+        "Fulong of Oolong,12,T Publishing,LEVEL_0,false,false,\"9780201309515|9788131700075\""
     })
     void objectMapperReturnsBookWhenInputIsValidJson(String seriesTitle,
                                                      String seriesNumber,
@@ -53,17 +99,17 @@ class BookTest extends ModelTest {
         boolean expectedPeerReviewed = Boolean.getBoolean(peerReviewed);
         List<String> expectedIsbn = convertIsbnStringToList(isbnList);
         String json = generatePublicationJson(
-                BOOK,
-                seriesTitle,
-                seriesNumber,
-                publisher,
-                level,
-                expectedOpenAccess,
-                expectedPeerReviewed,
-                expectedIsbn,
-                null,
-                null,
-                SAMPLE_LINKED_CONTEXT
+            BOOK,
+            seriesTitle,
+            seriesNumber,
+            publisher,
+            level,
+            expectedOpenAccess,
+            expectedPeerReviewed,
+            expectedIsbn,
+            null,
+            null,
+            SAMPLE_LINKED_CONTEXT
         );
         Book book = objectMapper.readValue(json, Book.class);
         assertEquals(seriesTitle, book.getSeriesTitle());
@@ -77,11 +123,11 @@ class BookTest extends ModelTest {
     @DisplayName("Book serializes expected json")
     @ParameterizedTest
     @CsvSource({
-            "A series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
-            ",, Full publisher details, LEVEL_2, true, true, \"9780201309515|9788131700075\"",
-            "A series title,,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
-            "Fulong of Oolong,12,T Publishing,LEVEL_0,false,false,\"9780201309515|9788131700075\"",
-            "A Marxist analysis of marking systems,6903,ACO,LEVEL_1,true,false,"
+        "A series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
+        ",, Full publisher details, LEVEL_2, true, true, \"9780201309515|9788131700075\"",
+        "A series title,,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075\"",
+        "Fulong of Oolong,12,T Publishing,LEVEL_0,false,false,\"9780201309515|9788131700075\"",
+        "A Marxist analysis of marking systems,6903,ACO,LEVEL_1,true,false,"
     })
     void objectMapperProducesProperlyFormattedJsonWhenInputIsBook(String seriesTitle,
                                                                   String seriesNumber,
@@ -90,33 +136,33 @@ class BookTest extends ModelTest {
                                                                   String openAccess,
                                                                   String peerReviewed,
                                                                   String isbnList) throws JsonProcessingException,
-            InvalidIsbnException {
+                                                                                          InvalidIsbnException {
         List<String> expectedIsbnList = convertIsbnStringToList(isbnList);
         boolean expectedOpenAccess = Boolean.getBoolean(openAccess);
         boolean expectedPeerReviewed = Boolean.getBoolean(peerReviewed);
         Level expectedLevel = Level.valueOf(level);
         Book book = new Book.Builder()
-                .withSeriesTitle(seriesTitle)
-                .withSeriesNumber(seriesNumber)
-                .withPublisher(publisher)
-                .withLevel(expectedLevel)
-                .withOpenAccess(expectedOpenAccess)
-                .withPeerReviewed(expectedPeerReviewed)
-                .withIsbnList(expectedIsbnList)
-                .withLinkedContext(SAMPLE_LINKED_CONTEXT)
-                .build();
+            .withSeriesTitle(seriesTitle)
+            .withSeriesNumber(seriesNumber)
+            .withPublisher(publisher)
+            .withLevel(expectedLevel)
+            .withOpenAccess(expectedOpenAccess)
+            .withPeerReviewed(expectedPeerReviewed)
+            .withIsbnList(expectedIsbnList)
+            .withLinkedContext(SAMPLE_LINKED_CONTEXT)
+            .build();
         String expectedJson = generatePublicationJson(
-                BOOK,
-                seriesTitle,
-                seriesNumber,
-                publisher,
-                level,
-                expectedOpenAccess,
-                expectedPeerReviewed,
-                expectedIsbnList,
-                null,
-                null,
-                SAMPLE_LINKED_CONTEXT
+            BOOK,
+            seriesTitle,
+            seriesNumber,
+            publisher,
+            level,
+            expectedOpenAccess,
+            expectedPeerReviewed,
+            expectedIsbnList,
+            null,
+            null,
+            SAMPLE_LINKED_CONTEXT
         );
         String actualJson = objectMapper.writeValueAsString(book);
         assertEquals(expectedJson, actualJson);
@@ -125,29 +171,15 @@ class BookTest extends ModelTest {
     @DisplayName("Book complains if ISBNs are invalid")
     @ParameterizedTest
     @CsvSource({
-            "Series title,123,Full publisher details,LEVEL_2,true,true,\"obviousNonsense|9788131700075\"",
-            "Series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|obviousNonsense\"",
-            "Series title,123,Full publisher details,LEVEL_2,true,true,\"9780201309515|9788131700075|obviousNonsense\""
+        "\"obviousNonsense|9788131700075\"",
+        "\"9780201309515|obviousNonsense\"",
+        "\"9780201309515|9788131700075|obviousNonsense\""
     })
-    void bookThrowsInvalidIsbnExceptionWhenIsbnIsInvalid(String seriesTitle,
-                                                         String seriesNumber,
-                                                         String publisher,
-                                                         String level,
-                                                         String openAccess,
-                                                         String peerReviewed,
-                                                         String isbnList) {
+    void bookThrowsInvalidIsbnExceptionWhenIsbnIsInvalid(String isbnList) {
 
         ArrayList<String> invalidIsbnList = new ArrayList<>(convertIsbnStringToList(isbnList));
 
-        Executable executable = () -> new Book.Builder()
-                .withSeriesTitle(seriesTitle)
-                .withSeriesNumber(seriesNumber)
-                .withPublisher(publisher)
-                .withLevel(Level.valueOf(level))
-                .withOpenAccess(Boolean.getBoolean(openAccess))
-                .withPeerReviewed(Boolean.getBoolean(peerReviewed))
-                .withIsbnList(invalidIsbnList)
-                .build();
+        Executable executable = () -> randomBook().copy().withIsbnList(invalidIsbnList).build();
 
         Exception exception = assertThrows(InvalidIsbnException.class, executable);
         String expectedMessage = String.format(InvalidIsbnException.ERROR_TEMPLATE, "obviousNonsense");
@@ -157,16 +189,8 @@ class BookTest extends ModelTest {
 
     @DisplayName("Book: Null ISBNs are handled gracefully")
     @Test
-    void bookReturnsEmptyListWhenIsbnsAreNull() throws InvalidIsbnException {
-        Book book = new Book.Builder()
-                .withSeriesTitle(null)
-                .withSeriesNumber(null)
-                .withLevel(Level.LEVEL_0)
-                .withPeerReviewed(false)
-                .withOpenAccess(false)
-                .withPublisher(null)
-                .withIsbnList(null)
-                .build();
+    void bookReturnsEmptyListWhenIsbnsAreNull() throws InvalidIsbnException, MalformedURLException {
+        Book book = randomBook().copy().withIsbnList(null).build();
 
         List<String> resultIsbnList = book.getIsbnList();
         assertThat(resultIsbnList, is(not(nullValue())));
@@ -175,17 +199,8 @@ class BookTest extends ModelTest {
 
     @DisplayName("Book: Empty ISBNs are handled gracefully")
     @Test
-    void bookReturnsEmptyListWhenIsbnListIsEmpty() throws InvalidIsbnException {
-        Book book = new Book.Builder()
-                .withSeriesTitle(null)
-                .withSeriesNumber(null)
-                .withLevel(Level.LEVEL_0)
-                .withPeerReviewed(false)
-                .withOpenAccess(false)
-                .withPublisher(null)
-                .withIsbnList(Collections.emptyList())
-                .build();
-
+    void bookReturnsEmptyListWhenIsbnListIsEmpty() throws InvalidIsbnException, MalformedURLException {
+        Book book = randomBook().copy().withIsbnList(Collections.emptyList()).build();
         List<String> resultIsbnList = book.getIsbnList();
         assertThat(resultIsbnList, is(not(nullValue())));
         assertThat(resultIsbnList, is(empty()));
@@ -195,23 +210,22 @@ class BookTest extends ModelTest {
     @Test
     void bookIsSerializedAndDeserializedWithLinkedContext() throws InvalidIsbnException, JsonProcessingException {
         Book actualBook = new Book.Builder()
-                .withLinkedContext(SAMPLE_LINKED_CONTEXT)
-                .build();
+            .withLinkedContext(SAMPLE_LINKED_CONTEXT)
+            .build();
 
         String expectedJson = generatePublicationJson(
-                BOOK,
-                null,
-                null,
-                null,
-                null,
-                false,
-                false,
-                null,
-                null,
-                null,
-                SAMPLE_LINKED_CONTEXT
+            BOOK,
+            null,
+            null,
+            null,
+            null,
+            false,
+            false,
+            null,
+            null,
+            null,
+            SAMPLE_LINKED_CONTEXT
         );
-
 
         String actualJson = objectMapper.writeValueAsString(actualBook);
         assertEquals(expectedJson, actualJson);
--- src/test/java/no/unit/nva/utils/RandomData.java
+++ src/test/java/no/unit/nva/utils/RandomData.java
@@ -2,13 +2,19 @@ package no.unit.nva.utils;
 
 import com.github.javafaker.Faker;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomData {
 
     public static final String EXAMPLE_HOST = "https://www.example.com/";
     public static final String PATH_DELIMITER = "/";
     public static final Random RANDOM = new Random();
+    public static final int FIRST_MULTIPLYING_FACTOR_FOR_ISBN_CALC = 1;
+    public static final int SECOND_MULTIPLYING_FACTOR_FOR_ISBN_CALC = 3;
     private static final Faker FAKER = Faker.instance();
 
     public static String randomString() {
@@ -19,7 +25,7 @@ public class RandomData {
         return FAKER.bool().bool();
     }
 
-    public static URI randomURI() {
+    public static URI randomUri() {
         return URI.create(EXAMPLE_HOST + randomWord() + PATH_DELIMITER + randomWord());
     }
 
@@ -27,7 +33,56 @@ public class RandomData {
         return values[RANDOM.nextInt(values.length)];
     }
 
+    public static int randomInt(int bound) {
+        return RANDOM.nextInt(bound);
+    }
+
+    public static String randomIsbn() {
+        List<Integer> isbnDigits = new ArrayList<>();
+        List<Integer> firstThreeDigits = List.of(9, 7, 8);
+        List<Integer> rest9Digits = randomDigits();
+        isbnDigits.addAll(firstThreeDigits);
+        isbnDigits.addAll(rest9Digits);
+
+        int lastDigit = calculateCheckDigit(isbnDigits);
+        if (isbnCannotBeValid(lastDigit)) {
+            return randomIsbn();
+        }
+        isbnDigits.add(lastDigit);
+        return isbnDigits.stream().map(Object::toString).collect(Collectors.joining());
+    }
+
+    private static boolean isbnCannotBeValid(int lastDigit) {
+        return lastDigit == 10;
+    }
+
     private static String randomWord() {
         return FAKER.lorem().word();
+    }
+
+    private static int calculateCheckDigit(List<Integer> isbnDigits) {
+        return 10 - calculateIsbnSum(isbnDigits) % 10;
+    }
+
+    private static List<Integer> randomDigits() {
+        return IntStream.range(0, 9).boxed()
+            .map(ignored -> randomIsbnDigit())
+            .collect(Collectors.toList());
+    }
+
+    private static int calculateIsbnSum(List<Integer> firstDigits) {
+        int sum = 0;
+        for (int index = 0; index < firstDigits.size(); index++) {
+            if (index % 2 == 0) {
+                sum += FIRST_MULTIPLYING_FACTOR_FOR_ISBN_CALC * firstDigits.get(index);
+            } else {
+                sum += SECOND_MULTIPLYING_FACTOR_FOR_ISBN_CALC * firstDigits.get(index);
+            }
+        }
+        return sum;
+    }
+
+    private static int randomIsbnDigit() {
+        return RandomData.randomInt(10);
     }
 }
