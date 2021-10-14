2fc9b3432f87802287e6d3e43a4138deb8d5a350
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-14 22:02:47 +0200

    linting

--- migration/src/test/java/no/unit/nva/datamodel/FunctionalTests.java
+++ migration/src/test/java/no/unit/nva/datamodel/FunctionalTests.java
@@ -44,7 +44,6 @@ public class FunctionalTests {
 
     @BeforeEach
     public void init() throws IOException {
-
         crateSampleProjectFolder();
         deleteExistingSerializations();
         buildSampleProject();
