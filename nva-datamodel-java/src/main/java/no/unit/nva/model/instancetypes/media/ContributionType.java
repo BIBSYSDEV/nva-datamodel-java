efc362c9768119b835f99495b80444aa8c8cfb20
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-10-21 08:53:36 +0200

    Use appropriately formatted data in enum

--- nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/media/ContributionType.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/media/ContributionType.java
@@ -1,5 +1,10 @@
 package no.unit.nva.model.instancetypes.media;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum ContributionType {
     INTERNET("Internet"),
     JOURNAL("Journal"),
@@ -14,7 +19,17 @@ public enum ContributionType {
         this.name = name;
     }
 
+    @JsonValue
     public String getName() {
         return name;
+    }
+
+
+    @JsonCreator
+    public static ContributionType fromString(String name) {
+        return Arrays.stream(values())
+            .filter(value -> value.getName().equals(name))
+            .findAny()
+            .orElseThrow();
     }
 }
