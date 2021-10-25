cda539b5dea79b99db6a95d6a0e2d6707ff64c9e
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-10-25 16:12:03 +0200

    Add UnconfirmedOrganization

---
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/UnconfirmedOrganization.java
@@ -0,0 +1,39 @@
+package no.unit.nva.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import nva.commons.core.JacocoGenerated;
+
+import java.util.Objects;
+
+public class UnconfirmedOrganization implements Agent {
+
+    public static final String NAME = "name";
+
+    @JsonProperty(NAME)
+    private final String name;
+
+    @JsonCreator
+    public UnconfirmedOrganization(@JsonProperty(NAME) String name) {
+        this.name = name;
+    }
+
+    @JacocoGenerated
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UnconfirmedOrganization)) {
+            return false;
+        }
+        UnconfirmedOrganization that = (UnconfirmedOrganization) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @JacocoGenerated
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
