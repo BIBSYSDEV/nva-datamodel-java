cdc10f026f478316ff0e7f7d4604770aa977a0e4
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-12-07 22:47:09 +0100

    first test for version compatibility

--- migration/src/test/java/no/unit/nva/datamodel/FunctionalTests.java
+++ migration/src/test/java/no/unit/nva/datamodel/FunctionalTests.java
@@ -1,6 +1,7 @@
 package no.unit.nva.datamodel;
 
 import static no.unit.nva.datamodel.migration.MigrationConfig.objectMapper;
+import static nva.commons.core.attempt.Try.attempt;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
@@ -20,7 +21,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.model.Publication;
 import nva.commons.core.Environment;
 import nva.commons.core.JsonUtils;
@@ -31,15 +35,15 @@ import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-@Tag("migrationTest")
 public class FunctionalTests {
 
     public static final String CURRENT_DATAMODEL_VERSION = new Environment().readEnv("DATAMODEL_VERSION");
-    public static final String PREVIOUS_DATAMODEL_VERSION = "0.14.8";
+    public static final String PREVIOUS_DATAMODEL_VERSION = "0.14.13";
     public static final String SERIALIZATIONS_SUBFOLDER = "serializations";
     public static final String SAMPLE_PROJECT_FOLDER_NAME = "sample-generation";
     public static final String DEPENDENCIES_FILE = "libs.versions.toml";
@@ -47,20 +51,28 @@ public class FunctionalTests {
     public static final int SOME_TIME_FOR_MAVEN_LOCAL_TO_REGISTER_CHANGES = 2000;
     public static final String HARDCODED_DATAMODEL_VERSION_PLACEHOLDER_IN_RESOURCE_FILE =
         "<DATAMODEL_VERSION_PLACEHOLDER>";
-    public static final String GRADLE_FOLDER_IN_PROJECS = "gradle";
+    public static final String GRADLE_FOLDER_IN_PROJECTS = "gradle";
     public static final String GRADLE_BUILD_COMMAND = "build";
     public static final String GRADLE_COMMAND_FOR_PUBLISHING_TO_MAVEN_LOCAL = "publishToMavenLocal";
     public static final String CURRENT_FOLDER = "";
+    public static final String DATAMODEL_DEPENDENCY_REGEX_FOR_REPLACEMENT =
+        "datamodel\\s*=\\s*\\{\\s*strictly\\s*=\\s*'[^']+'\\s*}";
+    public static final Pattern DATAMODEL_DEPENDENCY_REGEX_FOR_DETECTION =
+        Pattern.compile("[\\s\\S]*\\s*datamodel\\s*=\\s*\\{\\s*strictly\\s*=\\s*'[^']+'\\s*}[\\s\\S]*");
+
+    public static final String DATAMODEL_DEPENDENCY_NOT_FOUND_ERROR = "Could not find the datamodel dependency in the"
+                                                                      + " 'libs.versions.toml' file";
 
     @TempDir
     static File temporaryDir;
     static File sampleProjectCurrentVersion;
     static File sampleProjectPreviousVersion;
 
+
     @BeforeAll
     public static void init() throws IOException {
         setupTemporaryFolders();
-        buildWipNvaDatamodel();
+        publishCurrentVersionToMavenLocal();
         waitUntilLibraryHasBeenRegisteredInMavenLocal();
         buildSampleProject(CURRENT_DATAMODEL_VERSION, sampleProjectCurrentVersion);
         buildSampleProject(PREVIOUS_DATAMODEL_VERSION, sampleProjectPreviousVersion);
@@ -95,17 +107,40 @@ public class FunctionalTests {
     public void currentVersionSerializationsShouldBeDeserializedByCurrentVersion() throws FileNotFoundException {
         List<String> jsons = listSerializedPublications(sampleProjectCurrentVersion);
         for (String json : jsons) {
-            assertDoesNotThrow(() -> objectMapper.readValue(json, Publication.class));
+            assertDoesNotThrow(() -> deserializeWithCurrentVersion(json));
         }
     }
 
     @Tag("migrationTest")
     @Test
-    public void previousVersionSerializationsShouldBeDeserializedByCurrentVersion() throws FileNotFoundException {
+    public void currentVersionShouldProduceSameDeserializationWhenDeserializingOldAndNewSerializedVersions()
+        throws FileNotFoundException {
         List<String> jsons = listSerializedPublications(sampleProjectPreviousVersion);
-        for (String json : jsons) {
-            assertDoesNotThrow(() -> objectMapper.readValue(json, Publication.class));
+        var deserializedFromOldVersionSerialization = jsons.stream()
+            .map(this::deserializeWithCurrentVersion)
+            .collect(Collectors.toMap(Publication::getIdentifier, publication -> publication));
+        var newVersionSerializations = deserializedFromOldVersionSerialization.values().stream()
+            .map(this::serializeWithCurrentVersion)
+            .collect(Collectors.toList());
+        var deserializedFromNewVersionSerialization =  newVersionSerializations
+            .stream()
+            .map(this::deserializeWithCurrentVersion)
+            .collect(Collectors.toMap(Publication::getIdentifier, publication->publication));
+
+        var allIdentifiers =deserializedFromOldVersionSerialization.keySet();
+        for(SortableIdentifier identifier:allIdentifiers){
+            var deserializedFromOldVersion = deserializedFromOldVersionSerialization.get(identifier);
+            var deserializedFromNewVersion = deserializedFromNewVersionSerialization.get(identifier);
+            assertThat(deserializedFromNewVersion,is(equalTo(deserializedFromOldVersion)));
         }
+    }
+
+    private String serializeWithCurrentVersion(Publication value) {
+        return attempt(()->objectMapper.writeValueAsString(value)).orElseThrow();
+    }
+
+    private Publication deserializeWithCurrentVersion(String json) {
+        return attempt(()->objectMapper.readValue(json, Publication.class)).orElseThrow();
     }
 
     private static void setupTemporaryFolders() throws IOException {
@@ -129,6 +164,7 @@ public class FunctionalTests {
         BuildResult buildResult = GradleRunner.create()
             .withProjectDir(sampleProjectFile)
             .withArguments(GRADLE_BUILD_COMMAND)
+            .withDebug(true)
             .build();
         buildResult.getTasks().forEach(task -> assertThat(task.getOutcome(), is(not(equalTo(TaskOutcome.FAILED)))));
     }
@@ -141,7 +177,7 @@ public class FunctionalTests {
         }
     }
 
-    private static void buildWipNvaDatamodel() {
+    private static void publishCurrentVersionToMavenLocal() {
         BuildResult gradleRunner = GradleRunner.create()
             .withProjectDir(rootFolder())
             .withArguments(GRADLE_COMMAND_FOR_PUBLISHING_TO_MAVEN_LOCAL)
@@ -170,14 +206,29 @@ public class FunctionalTests {
     }
 
     private static void injectDatamodelVersion(File testingFolder, String version) throws IOException {
-        String libsVersionFile = IoUtils.stringFromResources(Path.of(DEPENDENCIES_FILE));
-        libsVersionFile = libsVersionFile.replaceAll(HARDCODED_DATAMODEL_VERSION_PLACEHOLDER_IN_RESOURCE_FILE, version);
-        File projectGradleFolder = new File(testingFolder, GRADLE_FOLDER_IN_PROJECS).getAbsoluteFile();
-        File newLibsFile = new File(projectGradleFolder, DEPENDENCIES_FILE);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(newLibsFile));
-        writer.write(libsVersionFile);
+        File projectGradleFolder = new File(testingFolder, GRADLE_FOLDER_IN_PROJECTS).getAbsoluteFile();
+        File dependenciesFile = new File(projectGradleFolder,DEPENDENCIES_FILE).getAbsoluteFile();
+        String dependenciesFileContents = IoUtils.stringFromFile(dependenciesFile.toPath());
+
+        assertThat(DATAMODEL_DEPENDENCY_NOT_FOUND_ERROR,
+                   DATAMODEL_DEPENDENCY_REGEX_FOR_DETECTION.matcher(dependenciesFileContents).matches(), is(true));
+        dependenciesFileContents=dependenciesFileContents.replaceFirst(DATAMODEL_DEPENDENCY_REGEX_FOR_REPLACEMENT, datamodelDependency(version));
+        System.out.println(dependenciesFileContents);
+
+        dependenciesFile.delete();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(dependenciesFile));
+        writer.write(dependenciesFileContents);
         writer.flush();
         writer.close();
+    }
+
+    private static String datamodelDependency(String version) {
+        return String.format( "%sdatamodel = {strictly = '%s'}%s",
+                              System.lineSeparator(),
+                              version,
+                              System.lineSeparator()
+        );
     }
 
     private List<String> listSerializedPublications(File folder) throws FileNotFoundException {
@@ -193,3 +244,4 @@ public class FunctionalTests {
         return jsons;
     }
 }
+
