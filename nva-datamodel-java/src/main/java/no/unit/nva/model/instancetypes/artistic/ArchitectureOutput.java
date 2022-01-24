b0d2c6ba2d892f117b3b05980aae8b6f15f821f7
brinxmat <789709+brinxmat@users.noreply.github.com>
2022-01-24 22:11:16 +0100

    Linting

--- nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/ArchitectureOutput.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/ArchitectureOutput.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(name = "Competition", value = Competition.class)
+    @JsonSubTypes.Type(name = "Competition", value = Competition.class)
 })
 public interface ArchitectureOutput {
 }
