a5ea6d6171e2e3d537b411c87526d143c78ca180
brinxmat <789709+brinxmat@users.noreply.github.com>
2022-01-31 05:06:17 +0100

    Remove non-existing subtype

--- nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/ArchitectureOutput.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/ArchitectureOutput.java
@@ -3,12 +3,10 @@ package no.unit.nva.model.instancetypes.artistic;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import no.unit.nva.model.instancetypes.artistic.realization.Competition;
-import no.unit.nva.model.instancetypes.artistic.realization.MentionOrPublication;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-    @JsonSubTypes.Type(name = "Competition", value = Competition.class),
-    @JsonSubTypes.Type(name = "MentionOrPublication", value = MentionOrPublication.class)
+    @JsonSubTypes.Type(name = "Competition", value = Competition.class)
 })
 public interface ArchitectureOutput {
 }
