e8822ff9a29c5e4a6ed699abe16a0df41cb879c8
brinxmat <789709+brinxmat@users.noreply.github.com>
2022-10-30 17:51:01 +0100

    Use a better check for array specification

--- nva-datamodel-java/src/test/java/no/unit/nva/PublicationContextTest.java
+++ nva-datamodel-java/src/test/java/no/unit/nva/PublicationContextTest.java
@@ -1,5 +1,6 @@
 package no.unit.nva;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import no.unit.nva.commons.json.JsonUtils;
@@ -7,19 +8,20 @@ import no.unit.nva.model.Publication;
 import no.unit.nva.model.testing.PublicationGenerator;
 import no.unit.nva.model.testing.PublicationInstanceBuilder;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -28,6 +30,7 @@ import static java.util.Collections.emptyList;
 import static nva.commons.core.attempt.Try.attempt;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -35,7 +38,6 @@ import static org.hamcrest.core.IsNull.nullValue;
 class PublicationContextTest {
 
     public static final ObjectMapper MAPPER = JsonUtils.dtoObjectMapper;
-    public static final String ENDS_WITH_S = ".*s$";
 
     public static Stream<Class<?>> publicationInstanceProvider() {
         return PublicationInstanceBuilder.listPublicationInstanceTypes().stream();
@@ -52,25 +54,7 @@ class PublicationContextTest {
         assertThat(serialized, not(containsString("jsonLdContext")));
     }
 
-    /**
-     * A test that will fail and cause issues due to it matching the plural s in a vain attempt
-     * to stop developers from adding list types without declaring it in as a @set the context object.
-     * @param type The PublicationInstance class
-     */
-    @ParameterizedTest
-    @MethodSource("publicationInstanceProvider")
-    void shouldSerializeAsRdfWithNoArrayPlurals(Class<?> type) {
-        var exceptions = Set.of("pages", "series",  "movements", "partOfSeries", 
-                "userAgreesToTermsAndConditions", "status", "approvalStatus");
-        var publication = generatePublicationWithContext(type);
-        var properties = extractAllRdfProperties(publication);
-        properties.removeAll(exceptions);
-        var testableList = properties.stream()
-                .filter(s -> s.matches(ENDS_WITH_S)).collect(Collectors.toList());
-        assertThat(testableList, is(emptyList()));
-    }
-
-    // TODO: actually filter the expected classes
+    // TODO: actually filter the expected classes from ontology
     @Test
     void shouldWriteEveryNvaClassAsFragmentUri() {
         var objectResources = generateListOfDistinctObjectClassesForEveryNvaPublicationType();
@@ -81,7 +65,79 @@ class PublicationContextTest {
         assertThat(nvaObjects, is(emptyList()));
     }
 
+    @Test
+    void shouldDefineEveryArrayPropertyAsContainerSet() {
+        var allNvaCollectionProperties = generateSetOfPropertiesThatHaveCollectionTypeForEveryNvaType();
+        var contextContainerSets = getAllContextContainerSetTerms();
+        assertThat(contextContainerSets, is(equalTo(allNvaCollectionProperties)));
+    }
+
     // TODO: test that every property and class is described in the ontology
+
+    private Set<String> getAllContextContainerSetTerms() {
+        var elements = getElementsFromJsonContextObject();
+        return extractPropertyKeysThatSpecifyContainerSets(elements);
+    }
+
+    private static HashSet<String> extractPropertyKeysThatSpecifyContainerSets(
+            Iterator<Map.Entry<String, JsonNode>> elements) {
+        var properties = new HashSet<String>();
+        while (elements.hasNext()) {
+            var current = elements.next();
+            if (isContainerSetSpecification(current)) {
+                properties.add(current.getKey());
+            }
+        }
+        return properties;
+    }
+
+    private static Iterator<Map.Entry<String, JsonNode>> getElementsFromJsonContextObject() {
+        var contextFromPublication = new Publication().getJsonLdContext();
+        var jsonLdContext = stringToTree(contextFromPublication);
+        return jsonLdContext.fields();
+    }
+
+    private static boolean isContainerSetSpecification(Map.Entry<String, JsonNode> current) {
+        return current.getValue().isObject()
+                && current.getValue().has("@container")
+                && current.getValue().get("@container").asText().equals("@set");
+    }
+
+    private Set<String> generateSetOfPropertiesThatHaveCollectionTypeForEveryNvaType() {
+        return publicationInstanceProvider()
+                .map(PublicationGenerator::randomPublication)
+                .map(PublicationContextTest::convertToJsonNode)
+                .map(PublicationContextTest::extractArrayFieldNames)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<String> extractArrayFieldNames(JsonNode jsonNode) {
+        var fields = jsonNode.fields();
+        var fieldNames = new HashSet<String>();
+        extractPPropertiesThaHaveArrayValue(fields, fieldNames);
+        return fieldNames;
+    }
+
+    private static void extractPPropertiesThaHaveArrayValue(
+            Iterator<Map.Entry<String, JsonNode>> fields, HashSet<String> fieldNames) {
+        while (fields.hasNext()) {
+            var current = fields.next();
+            if (current.getValue().isArray()) {
+                fieldNames.add(current.getKey());
+                iterateArray(current.getValue().elements(), fieldNames);
+            }
+            if (current.getValue().isObject()) {
+                extractPPropertiesThaHaveArrayValue(current.getValue().fields(), fieldNames);
+            }
+        }
+    }
+
+    private static void iterateArray(Iterator<JsonNode> elements, HashSet<String> fieldNames) {
+        while (elements.hasNext()) {
+            extractPPropertiesThaHaveArrayValue(elements.next().fields(), fieldNames);
+        }
+    }
 
     private static Set<String> generateListOfDistinctObjectClassesForEveryNvaPublicationType() {
         return generateStatementsForEveryPublicationType().stream()
@@ -100,10 +156,10 @@ class PublicationContextTest {
         var publications = publicationInstanceProvider()
                 .map(PublicationContextTest::generatePublicationWithContext)
                 .collect(Collectors.toList());
-        return addAllResourcesToModel(publications);
+        return getDistinctStatementsOfResources(publications);
     }
 
-    private static Set<Statement> addAllResourcesToModel(List<InputStream> inputStreams) {
+    private static Set<Statement> getDistinctStatementsOfResources(List<InputStream> inputStreams) {
         var model = ModelFactory.createDefaultModel();
         inputStreams.forEach(inputStream -> RDFDataMgr.read(model, inputStream, Lang.JSONLD11));
         return model.listStatements().toSet();
@@ -117,25 +173,22 @@ class PublicationContextTest {
         return item.startsWith("https://nva.sikt.no/");
     }
 
-    private static Set<String> extractAllRdfProperties(InputStream inputStream) {
-        return getPublicationStatements(inputStream).stream()
-                        .map(Statement::getPredicate)
-                        .map(Property::getLocalName)
-                        .collect(Collectors.toSet());
-    }
-
-    private static Set<Statement> getPublicationStatements(InputStream inputStream) {
-        return addAllResourcesToModel(List.of(inputStream));
-    }
-
     private static InputStream generatePublicationWithContext(Class<?> type) {
         var publication = PublicationGenerator.randomPublication(type);
         return new ByteArrayInputStream((addContextObjectToPublication(publication)));
     }
 
     private static byte[] addContextObjectToPublication(Publication publication) {
-        var jsonNode = (ObjectNode) attempt(() -> MAPPER.valueToTree(publication)).orElseThrow();
-        jsonNode.set("@context", attempt(() -> MAPPER.readTree(publication.getJsonLdContext())).orElseThrow());
+        var jsonNode = (ObjectNode) convertToJsonNode(publication);
+        jsonNode.set("@context", stringToTree(publication.getJsonLdContext()));
         return jsonNode.toString().getBytes();
+    }
+
+    private static JsonNode stringToTree(String string) {
+        return attempt(() -> MAPPER.readTree(string)).orElseThrow();
+    }
+
+    private static JsonNode convertToJsonNode(Publication instance) {
+        return (JsonNode) attempt(() -> MAPPER.valueToTree(instance)).orElseThrow();
     }
 }
