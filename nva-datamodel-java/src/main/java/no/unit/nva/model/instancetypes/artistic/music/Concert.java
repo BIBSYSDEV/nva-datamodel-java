b63237dbaec138602cd84d9499692a4c4a638a8b
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2023-04-04 10:45:47 +0200

    change partOfSeries boolean to series String (#367)

--- nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/music/Concert.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/instancetypes/artistic/music/Concert.java
@@ -16,7 +16,7 @@ public class Concert implements MusicPerformanceManifestation {
     public static final String EXTENT_FIELD = "extent";
     public static final String DESCRIPTION_FIELD = "description";
     public static final String CONCERT_PROGRAMME_FIELD = "concertProgramme";
-    public static final String PART_OF_SERIES_FIELD = "partOfSeries";
+    public static final String SERIES_FIELD = "series";
 
     @JsonProperty(PLACE_FIELD)
     private final UnconfirmedPlace place;
@@ -28,8 +28,8 @@ public class Concert implements MusicPerformanceManifestation {
     private final String description;
     @JsonProperty(CONCERT_PROGRAMME_FIELD)
     private final List<MusicalWorkPerformance> concertProgramme;
-    @JsonProperty(PART_OF_SERIES_FIELD)
-    private final boolean partOfSeries;
+    @JsonProperty(SERIES_FIELD)
+    private final String series;
 
     @JsonCreator
     public Concert(@JsonProperty(PLACE_FIELD) UnconfirmedPlace place,
@@ -37,13 +37,13 @@ public class Concert implements MusicPerformanceManifestation {
                    @JsonProperty(EXTENT_FIELD) String extent,
                    @JsonProperty(DESCRIPTION_FIELD) String description,
                    @JsonProperty(CONCERT_PROGRAMME_FIELD) List<MusicalWorkPerformance> concertProgramme,
-                   @JsonProperty(PART_OF_SERIES_FIELD) boolean partOfSeries) {
+                   @JsonProperty(SERIES_FIELD) String series) {
         this.place = place;
         this.time = time;
         this.extent = extent;
         this.description = description;
         this.concertProgramme = nullListAsEmpty(concertProgramme);
-        this.partOfSeries = partOfSeries;
+        this.series = series;
     }
 
     public UnconfirmedPlace getPlace() {
@@ -66,8 +66,8 @@ public class Concert implements MusicPerformanceManifestation {
         return concertProgramme;
     }
 
-    public boolean isPartOfSeries() {
-        return partOfSeries;
+    public String getSeries() {
+        return series;
     }
 
     @JacocoGenerated
@@ -76,22 +76,21 @@ public class Concert implements MusicPerformanceManifestation {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof Concert)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         Concert concert = (Concert) o;
-        return isPartOfSeries() == concert.isPartOfSeries()
-                && Objects.equals(getPlace(), concert.getPlace())
-                && Objects.equals(getTime(), concert.getTime())
-                && Objects.equals(getExtent(), concert.getExtent())
-                && Objects.equals(getDescription(), concert.getDescription())
-                && Objects.equals(getConcertProgramme(), concert.getConcertProgramme());
+        return Objects.equals(getPlace(), concert.getPlace())
+               && Objects.equals(getTime(), concert.getTime())
+               && Objects.equals(getExtent(), concert.getExtent())
+               && Objects.equals(getDescription(), concert.getDescription())
+               && Objects.equals(getConcertProgramme(), concert.getConcertProgramme())
+               && Objects.equals(getSeries(), concert.getSeries());
     }
 
     @JacocoGenerated
     @Override
     public int hashCode() {
-        return Objects.hash(getPlace(), getTime(), getExtent(), getDescription(),
-                getConcertProgramme(), isPartOfSeries());
+        return Objects.hash(getPlace(), getTime(), getExtent(), getDescription(), getConcertProgramme(), getSeries());
     }
 }
