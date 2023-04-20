d42abec9924802f919c55d1166c29a5fac9618ff
Bruno Saxoni <789709+brinxmat@users.noreply.github.com>
2023-04-20 13:47:35 +0200

    Migrate isbn to isbnList (#374)
    * Migrate isbn to isbnList
    
    * Use illegal argument exception

---
+++ nva-datamodel-java/src/test/java/no/unit/nva/model/instancetypes/artistic/literaryarts/manifestation/LiteraryArtsManifestationMigrationTest.java
@@ -0,0 +1,30 @@
+package no.unit.nva.model.instancetypes.artistic.literaryarts.manifestation;
+
+import org.junit.jupiter.api.Test;
+
+import static no.unit.nva.model.testing.PublicationContextBuilder.randomPublishingHouse;
+import static no.unit.nva.model.testing.PublicationInstanceBuilder.randomMonographPages;
+import static no.unit.nva.model.testing.RandomUtils.randomPublicationDate;
+import static no.unit.nva.testutils.RandomDataGenerator.randomInteger;
+import static no.unit.nva.testutils.RandomDataGenerator.randomIsbn13;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+@Deprecated
+class LiteraryArtsManifestationMigrationTest {
+
+    @Test
+    void shouldCreateLiteraryArtsMonographWhenIsbnIsString() {
+        var literaryArtsMonograph = new LiteraryArtsMonograph(randomPublishingHouse(), randomPublicationDate(),
+                randomIsbn13(), randomMonographPages());
+        assertThat(literaryArtsMonograph.getIsbnList(), hasSize(1));
+    }
+
+
+    @Test
+    void shouldCreateLiteraryArtsAudioVisualWhenIsbnIsString() {
+        var literaryArtsAudioVisual = new LiteraryArtsAudioVisual(LiteraryArtsAudioVisualSubtype.PODCAST,
+                randomPublishingHouse(), randomPublicationDate(), randomIsbn13(), randomInteger());
+        assertThat(literaryArtsAudioVisual.getIsbnList(), hasSize(1));
+    }
+}
