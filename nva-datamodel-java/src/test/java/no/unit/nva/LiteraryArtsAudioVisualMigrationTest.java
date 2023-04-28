b08123f4433a86c39c3b8f8bd637c079f810809b
Bruno Saxoni <789709+brinxmat@users.noreply.github.com>
2023-04-28 12:32:51 +0200

    Fix isbn migration bug (#386)
    * Fix isbn migration bug
    
    * Format string concat

---
+++ nva-datamodel-java/src/test/java/no/unit/nva/LiteraryArtsAudioVisualMigrationTest.java
@@ -0,0 +1,65 @@
+package no.unit.nva;
+
+import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.model.instancetypes.artistic.literaryarts.manifestation.LiteraryArtsAudioVisual;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@Deprecated
+public class LiteraryArtsAudioVisualMigrationTest {
+    public static Stream<String> literaryArtsAudioVisualProvider() {
+        return Stream.of(
+                generateOld(),
+                generateNew()
+        );
+    }
+
+    private static String generateNew() {
+        return "{\n"
+                + "          \"type\" : \"LiteraryArtsAudioVisual\",\n"
+                + "          \"subtype\" : \"RadioPlay\",\n"
+                + "          \"publisher\" : {\n"
+                + "            \"type\" : \"UnconfirmedPublisher\",\n"
+                + "            \"name\" : \"rYgvX1EuiAFLb\"\n"
+                + "          },\n"
+                + "          \"publicationDate\" : {\n"
+                + "            \"type\" : \"PublicationDate\",\n"
+                + "            \"year\" : \"hJ9lbyvYn0VvE5oYHw\",\n"
+                + "            \"month\" : \"agqDekENHtR2Gy\",\n"
+                + "            \"day\" : \"rPHYfsH5VIzz\"\n"
+                + "          },\n"
+                + "          \"isbnList\" : [ \"9780099470434\" ],\n"
+                + "          \"extent\" : 1040853105\n"
+                + "        }";
+    }
+
+    private static String generateOld() {
+        return "{\n"
+                + "          \"type\" : \"LiteraryArtsAudioVisual\",\n"
+                + "          \"subtype\" : \"RadioPlay\",\n"
+                + "          \"publisher\" : {\n"
+                + "            \"type\" : \"UnconfirmedPublisher\",\n"
+                + "            \"name\" : \"rYgvX1EuiAFLb\"\n"
+                + "          },\n"
+                + "          \"publicationDate\" : {\n"
+                + "            \"type\" : \"PublicationDate\",\n"
+                + "            \"year\" : \"hJ9lbyvYn0VvE5oYHw\",\n"
+                + "            \"month\" : \"agqDekENHtR2Gy\",\n"
+                + "            \"day\" : \"rPHYfsH5VIzz\"\n"
+                + "          },\n"
+                + "          \"isbn\" : \"9780099470434\",\n"
+                + "          \"extent\" : 1040853105\n"
+                + "        }";
+    }
+
+    @ParameterizedTest
+    @MethodSource("literaryArtsAudioVisualProvider")
+    void shouldConstructObjectFromJson(String input) {
+        assertDoesNotThrow(() -> JsonUtils.dtoObjectMapper.readValue(input, LiteraryArtsAudioVisual.class));
+    }
+}
