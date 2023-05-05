9df5e08ba950bffc2d9bca92a37abcb2c962f34f
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-05-05 15:58:29 +0200

    Add full document tests

--- nva-datamodel-java/src/test/java/no/unit/nva/MusicPerformanceMedtiaTypeOtherMigrationTest.java
+++ nva-datamodel-java/src/test/java/no/unit/nva/MusicPerformanceMedtiaTypeOtherMigrationTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Deprecated
-class MusicPerformanceMedtiaTypeOtherMigrationTest {
+class MusicPerformanceMediaTypeOtherMigrationTest {
 
     @Test
     void shouldNotThrowWhenInputIsOldStyleMusicPerformance() {
