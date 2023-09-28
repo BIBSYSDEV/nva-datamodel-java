d5f4aaa76e5b8ef450d575a891a48465726ba73b
anetteOlli <anette.olli.siiri@sikt.no>
2023-09-28 11:27:51 +0200

    NP-45333-added-publication-note-field, code-review

--- nva-datamodel-java/src/main/java/no/unit/nva/model/PublicationNote.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/PublicationNote.java
@@ -4,8 +4,8 @@ package no.unit.nva.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-
+@Deprecated
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public record PublicationNote( String publicationNoteMessage) {
+public record PublicationNote(String publicationNoteMessage) {
 }
