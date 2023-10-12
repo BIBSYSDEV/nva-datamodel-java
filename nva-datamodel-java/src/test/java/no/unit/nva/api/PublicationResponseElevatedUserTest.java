fc49934c35f3c836b2a19a612a04dc24d157c1de
anetteOlli <anette.olli.siiri@sikt.no>
2023-10-12 13:14:02 +0200

    expose-publication-notes-in-publication-response-for-elevated-users

---
+++ nva-datamodel-java/src/test/java/no/unit/nva/api/PublicationResponseElevatedUserTest.java
@@ -0,0 +1,33 @@
+package no.unit.nva.api;
+
+import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValuesIgnoringFields;
+import static no.unit.nva.model.testing.PublicationGenerator.randomPublication;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import java.util.List;
+import java.util.Set;
+import no.unit.nva.model.Publication;
+import no.unit.nva.model.PublicationNote;
+import org.junit.jupiter.api.Test;
+
+public class PublicationResponseElevatedUserTest {
+
+    @Test
+    void shouldReceivePublicationNotesWhenConvertingFromPublication() {
+        var publication = randomPublication();
+        publication.setPublicationNotes(List.of(new PublicationNote(randomString())));
+        var publicationResponseForElevatedUsers = PublicationResponseElevatedUser.fromPublication(publication);
+        assertThat(publicationResponseForElevatedUsers.getPublicationNotes(),
+                   is(equalTo(publication.getPublicationNotes())));
+    }
+
+    @Test
+    void staticConstructorShouldReturnPublicationResponseForElevatedUsersWithoutUnexpectedLossOfInformation() {
+        Publication publication = randomPublication();
+        assertThat(publication,doesNotHaveEmptyValuesIgnoringFields(Set.of("doiRequest")));
+        var publicationResponse = PublicationResponseElevatedUser.fromPublication(publication);
+        assertThat(publicationResponse,doesNotHaveEmptyValuesIgnoringFields(Set.of("doiRequest")));
+    }
+}
