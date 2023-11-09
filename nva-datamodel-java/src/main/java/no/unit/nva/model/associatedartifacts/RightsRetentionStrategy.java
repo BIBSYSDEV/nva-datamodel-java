a11e2ccad1ae76262da3d6689d2bc6efd8c36a5b
Eyob Teweldemedhin <eyob.teweldemedhin@sikt.no>
2023-11-09 15:33:18 +0100

    Codacy fix.

--- nva-datamodel-java/src/main/java/no/unit/nva/model/associatedartifacts/RightsRetentionStrategy.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/associatedartifacts/RightsRetentionStrategy.java
@@ -4,18 +4,16 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
- * The RightsRetentionStrategy interface represents the strategy for rights retention.
- * It makes use of Jackson annotations to handle JSON serialization and deserialization into
- * the correct implementing types: CustomerRightsRetentionStrategy, OverriddenRightsRetentionStrategy,
- * and NullRightsRetentionStrategy.
+ * The RightsRetentionStrategy interface represents the strategy for rights retention. It makes use of Jackson
+ * annotations to handle JSON serialization and deserialization into the correct implementing types:
+ * CustomerRightsRetentionStrategy, OverriddenRightsRetentionStrategy, and NullRightsRetentionStrategy.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
     @JsonSubTypes.Type(name = CustomerRightsRetentionStrategy.TYPE_NAME, value = CustomerRightsRetentionStrategy.class),
     @JsonSubTypes.Type(
-        name = OverriddenRightsRetentionStrategy.TYPE_NAME,
-        value = OverriddenRightsRetentionStrategy.class
-    ),
+        name = NullRightsRetentionStrategy.TYPE_NAME,
+        value = NullRightsRetentionStrategy.class),
     @JsonSubTypes.Type(name = NullRightsRetentionStrategy.TYPE_NAME, value = NullRightsRetentionStrategy.class),
 })
 public interface RightsRetentionStrategy {
