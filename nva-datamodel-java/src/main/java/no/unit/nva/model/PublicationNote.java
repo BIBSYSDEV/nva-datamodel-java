f71aefe9832914fe576c597334cd6ed9bb1400d9
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2024-02-16 13:31:04 +0100

    added-fields-unpublished-note (#466)
    * added-fields-unpublished-note, implemented
    
    * bumped version
    
    * added-fields-unpublished-note, added alias
    
    * added-fields-unpublished-note, code-review

--- nva-datamodel-java/src/main/java/no/unit/nva/model/PublicationNote.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/PublicationNote.java
@@ -1,8 +1,17 @@
 package no.unit.nva.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Deprecated
 @JsonTypeName("PublicationNote")
-public record PublicationNote(String publicationNoteMessage) implements PublicationNoteBase {
+public class PublicationNote extends PublicationNoteBase {
+
+    @JsonCreator
+    public PublicationNote(@JsonProperty("note") @JsonAlias({"publicationNoteMessage"}) String note) {
+        super(note);
+    }
+
 }
