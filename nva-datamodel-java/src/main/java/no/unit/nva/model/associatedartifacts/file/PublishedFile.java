ffa3f8f28fa48157d9c7aaee24b8c0c5cd0ee84c
Joachim Jørgensen <joachimwhj@gmail.com>
2024-05-30 14:34:19 +0200

    NP-46821 Add copy method with builder to files (#488)
    * NP-46821 Add copy method with builder to files
    
    * Remove unused import
    
    * Add missing field-postfix, use instance first in .equals(), and fix spelling error

--- nva-datamodel-java/src/main/java/no/unit/nva/model/associatedartifacts/file/PublishedFile.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/associatedartifacts/file/PublishedFile.java
@@ -16,9 +16,6 @@ import no.unit.nva.model.associatedartifacts.RightsRetentionStrategy;
 public class PublishedFile extends File {
 
     public static final String TYPE = "PublishedFile";
-    public static final String PUBLISHED_DATE = "publishedDate";
-    @JsonProperty(PUBLISHED_DATE)
-    private final Instant publishedDate;
 
     /**
      * Constructor for no.unit.nva.file.model.File objects. A file object is valid if it has a license or is explicitly
@@ -48,20 +45,15 @@ public class PublishedFile extends File {
         @JsonProperty(ADMINISTRATIVE_AGREEMENT_FIELD) boolean administrativeAgreement,
         @JsonProperty(PUBLISHER_VERSION_FIELD) @JsonAlias(PUBLISHER_AUTHORITY_FIELD) Object publishedVersion,
         @JsonProperty(EMBARGO_DATE_FIELD) Instant embargoDate,
-        @JsonProperty(RIGTHTS_RETENTION_STRATEGY) RightsRetentionStrategy rightsRetentionStrategy,
+        @JsonProperty(RIGHTS_RETENTION_STRATEGY) RightsRetentionStrategy rightsRetentionStrategy,
         @JsonProperty(LEGAL_NOTE_FIELD) String legalNote,
-        @JsonProperty(PUBLISHED_DATE) Instant publishedDate,
-        @JsonProperty(UPLOAD_DETAILS) UploadDetails uploadDetails) {
+        @JsonProperty(PUBLISHED_DATE_FIELD) Instant publishedDate,
+        @JsonProperty(UPLOAD_DETAILS_FIELD) UploadDetails uploadDetails) {
         super(identifier, name, mimeType, size, license, administrativeAgreement, publishedVersion,
-              embargoDate, rightsRetentionStrategy, legalNote, uploadDetails);
-        this.publishedDate = publishedDate;
+              embargoDate, rightsRetentionStrategy, legalNote, publishedDate, uploadDetails);
         if (administrativeAgreement) {
             throw new IllegalStateException("An administrative agreement is not publishable");
         }
-    }
-
-    public Instant getPublishedDate() {
-        return publishedDate;
     }
 
     @Override
@@ -72,5 +64,21 @@ public class PublishedFile extends File {
     @Override
     public boolean isVisibleForNonOwner() {
         return !isAdministrativeAgreement() && fileDoesNotHaveActiveEmbargo();
+    }
+
+    @Override
+    public Builder copy() {
+        return builder()
+                   .withIdentifier(this.getIdentifier())
+                   .withName(this.getName())
+                   .withMimeType(this.getMimeType())
+                   .withSize(this.getSize())
+                   .withLicense(this.getLicense())
+                   .withAdministrativeAgreement(this.isAdministrativeAgreement())
+                   .withPublisherVersion(this.getPublisherVersion())
+                   .withEmbargoDate(this.getEmbargoDate().orElse(null))
+                   .withRightsRetentionStrategy(this.getRightsRetentionStrategy())
+                   .withLegalNote(this.getLegalNote())
+                   .withUploadDetails(this.getUploadDetails());
     }
 }
