ddf2ed0f736d4006246e1cff250efb7ac90216df
Kir Truhacev <kir.truhacev@sikt.no>
2024-06-20 13:42:30 +0200

    adding missing methods to ImportSource

--- nva-datamodel-java/src/main/java/no/unit/nva/model/ImportSource.java
+++ nva-datamodel-java/src/main/java/no/unit/nva/model/ImportSource.java
@@ -3,9 +3,12 @@ package no.unit.nva.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.util.Objects;
+import no.unit.nva.commons.json.JsonSerializable;
+import nva.commons.core.JacocoGenerated;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public class ImportSource {
+public class ImportSource implements JsonSerializable {
 
     public static final String NO_ARCHIVE = null;
     public static final String SOURCE_FIELD = "source";
@@ -14,8 +17,7 @@ public class ImportSource {
     private final String archive;
 
     @JsonCreator
-    public ImportSource(@JsonProperty(SOURCE_FIELD) Source source,
-                        @JsonProperty(ARCHIVE_FIELD) String archive) {
+    public ImportSource(@JsonProperty(SOURCE_FIELD) Source source, @JsonProperty(ARCHIVE_FIELD) String archive) {
         this.source = source;
         this.archive = archive;
     }
@@ -28,12 +30,35 @@ public class ImportSource {
         return new ImportSource(source, NO_ARCHIVE);
     }
 
+    @JacocoGenerated
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, archive);
+    }
+
+    @JacocoGenerated
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ImportSource that)) {
+            return false;
+        }
+        return source == that.source && Objects.equals(archive, that.archive);
+    }
+
     public Source getSource() {
         return source;
     }
 
     public String getArchive() {
         return archive;
+    }
+
+    @Override
+    public String toString() {
+        return this.toJsonString();
     }
 
     public enum Source {
